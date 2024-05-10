7f3c37ecf84f933ba810e9c5cd83ef0647276ce7
Kader ZKR <abdelkader.zekraoui1@gmail.com>
2024-05-10 18:21:08 +0100

    Updated navbar css styling

--- hambureact/src/App.js
+++ hambureact/src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import "./components/Navbar.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
+      <Navbar />
     </div>
   );
 }
